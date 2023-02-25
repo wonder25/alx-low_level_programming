d1be9dce5686a5c9b6b194a8becc8f1e452ed825
wonder25 <wonderfulmwinzi14@gmail.com>
2023-02-25 03:15:10 +0300

    fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - fizz buzz challenge
+ * main - prints numbers from 1-100
  * 3 multiples print fizz instead of the number
  * 5 multiples print buzz instead of the number
  * 3 & 5 multiples print FizzBuzz instead of the number
@@ -18,21 +18,13 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a == 100)
-		{
 			printf("%s", b);
-		}
 		else if (a % 3 == 0)
-		{
 			printf("%s ", f);
-		}
 		else if (a % 5 == 0)
-		{
 			printf("%s ", b);
-		}
 		else if ((a % 3 == 0) && (a % 5 == 0))
-		{
 			printf("%s ", fb);
-		}
 		else
 			printf("%d ", a);
 	}
