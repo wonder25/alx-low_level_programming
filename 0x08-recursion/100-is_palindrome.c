f33f67de4ead5ac1e1729e424f5fdd796e3ee3fe
wonder25 <wonderfulmwinzi14@gmail.com>
2023-03-09 23:44:56 +0300

    function that returns 1 if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int check_emp(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrom
+ * @s: string to reverse
+ * Return: return 1 if it is empty, return 0 it's not empty
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_emp(s, 0, _strlen_recursion(s)));
+}
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string to calculate the length
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_emp - checks characters recursively for palindrome
+ * @s: string to check
+ * @i:iterator
+ * @len: length of the string
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_emp(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_emp(s, i + 1, len - 1));
+}
