4894ad7b2a736b75e9a91b7934be4380a65a43ef
wonder25 <wonderfulmwinzi14@gmail.com>
2023-03-13 14:23:49 +0300

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (c = 1; c < argc; c++)
 	{
-		if (argv[c] < '0' || argv[c] > '9')
+		if (*argv[c] < '0' || *argv[c] > '9')
 		{
 			printf("Error\n");
 			return (1);
