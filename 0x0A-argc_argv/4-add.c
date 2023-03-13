950f7841a6be3f7e3225693cfc9017eaa1e09d7e
wonder25 <wonderfulmwinzi14@gmail.com>
2023-03-13 15:24:03 +0300

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 /**
- * main - function that adds positive numbers
+ * main - adds positive numbers
  * @argc: number of arguments
  * @argv: string to be used
- * Return: always 0
+ * Return: always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int c;
+	int c, b, sum = 0;
 
 	for (c = 1; c < argc; c++)
 	{
-		if (*argv[c] != '\0')
+		for (b = 0; argv[c][b] != '\0'; b++)
 		{
-			if (*argv[c] < '0' || *argv[c] > '9')
+			if (argv[c][b] < '0' || argv[c][b] > '9')
 			{
 				printf("Error\n");
 				return (1);
@@ -28,5 +27,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", sum);
+
 	return (0);
 }
