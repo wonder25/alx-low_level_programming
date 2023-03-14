c8fe342e4a470954cbadee5b84fc0074fd07a614
wonder25 <wonderfulmwinzi14@gmail.com>
2023-03-14 20:47:32 +0300

    duplicates strings and allocates memory to new string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * _strdup - function returns a pointer to a new string,
+ * which is a duplicate of the string
+ * @str: string to be used.
+ * Return: always 0
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *str1;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	str1 = malloc(sizeof(char) * (len + 1));
+
+	if (str1 == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		str1[i] = str[i];
+	}
+	return (str1);
+
+	free(str1);
+}
