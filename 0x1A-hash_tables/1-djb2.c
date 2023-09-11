d252f4b6d24b998d3c60c75827e53bc3fe9313e8
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-10 23:33:57 -0700

    implements the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,12 +8,11 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
+
 	hash = 5381;
 	while ((c = *str++))
-    	{
-        	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    	}
-    	return (hash);
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
-
